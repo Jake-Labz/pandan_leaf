730a02a2b08033c05e3abb26776cec011657e419
Jake Lawrence <jake.thelabrat@gmail.com>
2013-02-12 21:14:28 +0000

    First commit

---
+++ js/js.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+    $('#menu_starters').click(function() {
+    	if ($("#mains_nav").is(":hidden")) {
+    		$('#start_nav').slideDown(400);
+    	} else {
+    $('#start_nav').delay(600).slideDown(400);
+    $('#mains_nav, .ma, .nd, .ds').slideUp(400);
+		}
+	$('.m_menu').removeClass('selected');
+    return false;
+  });
+});
+
+$(document).ready(function() {
+    $('#menu_mains').click(function() {
+    	if ($("#start_nav").is(":hidden")) {
+    		$('#mains_nav').slideDown(400);
+    		$('s_menu').removeClass('selected');
+    	} else {
+    $('#mains_nav').delay(600).slideDown(400);
+    $('#start_nav, .st, .nd, .ds').slideUp(400);
+    $('.s_menu').removeClass('selected');
+		}
+    return false;
+  });
+});
+
+$(document).ready(function() {
+    $('.menu').click(function() {
+    $('.menu').removeClass('selected');  
+	$(this).addClass('selected');
+    return false;
+  });
+});
+
+$(document).ready(function() {
+    $('.s_menu, .m_menu').click(function() {
+    $('.s_menu, .m_menu').removeClass('selected');  
+	$(this).addClass('selected');
+    return false;
+  });
+});
+
+
+
+$(document).ready(function() {
+    $('#menu_noodles, #menu_desserts').click(function() {
+    $('#mains_nav, #start_nav, .menu_list').slideUp(250);
+    return false;
+  });
+});
+
+$(document).ready(function() {
+    $('#menu_st_meat').click(function() {
+    $('#starters_meat').slideDown(400);
+    return false;
+  });
+});
+
+$(document).ready(function() {
+    $('#menu_ma_curries').click(function() {
+    	if ($("#mains_meat, #mains_seafood, #mains_veg").is(":hidden")) {
+    		$('#mains_curries').slideDown(400);
+    	} else {
+    		$('#mains_curries').delay(600).slideDown(400);
+    		$("#mains_meat, #mains_seafood, #mains_veg").slideUp(400);
+    	}
+    return false;
+  });
+});
+
+$(document).ready(function() {
+    $('#menu_ma_meat').click(function() {
+    	if ($("#mains_curries, #mains_seafood, #mains_veg").is(":hidden")) {
+    		$('#mains_meat').slideDown(400);
+    	} else {
+    		$('#mains_meat').delay(600).slideDown(400);
+    		$("#mains_curries, #mains_seafood, #mains_veg").slideUp(400);
+    	}
+    return false;
+  });
+});
